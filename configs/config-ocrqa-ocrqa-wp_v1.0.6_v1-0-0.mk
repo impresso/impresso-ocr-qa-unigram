13b1b8e3354f51e3954310ba17e69c1090334db4
Simon Clematide <simon.clematide@uzh.ch>
2025-02-14 18:02:54 +0100

    feat: Add S3 bucket configuration for OCR QA pipeline

--- configs/config-ocrqa-ocrqa-wp_v1.0.6_v1-0-0.mk
+++ configs/config-ocrqa-ocrqa-wp_v1.0.6_v1-0-0.mk
@@ -1,3 +1,5 @@
+S3_BUCKET_OCRQA := 42-processed-data-final
+
 OCRQA_BLOOMFILTERS_OPTION := hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.6-de.bloom hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.6-fr.bloom
 
 OCRQA_MIN_SUBTOKENS_OPTION := --min-subtokens 10

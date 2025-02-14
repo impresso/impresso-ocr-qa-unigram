df45764059b8adeff35262ee21c0118069fecbe9
Simon Clematide <simon.clematide@uzh.ch>
2025-02-14 17:53:04 +0100

    feat: Add new configuration file for OCR QA pipeline with bloom filters and minimum subtokens

---
+++ configs/config-ocrqa-ocrqa-wp_v1.0.6_v1-0-0.mk
@@ -0,0 +1,5 @@
+OCRQA_BLOOMFILTERS_OPTION := hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.6-de.bloom hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.6-fr.bloom
+
+OCRQA_MIN_SUBTOKENS_OPTION := --min-subtokens 10
+
+MODEL_ID_OCRQA := wp_v1.0.6

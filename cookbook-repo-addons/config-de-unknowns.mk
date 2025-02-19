8a63d62615a4d1829db1ba1599c815963ea7845a
Simon Clematide <simon.clematide@uzh.ch>
2025-02-19 01:04:02 +0100

    chore: Update subproject commit reference and modify OCR QA pipeline configuration

--- cookbook-repo-addons/config-de-unknowns.mk
+++ cookbook-repo-addons/config-de-unknowns.mk
@@ -1,11 +1,14 @@
 # configuration file for running the impresso-ocr-qa-unigram pipeline in
 # --verbose-output mode for collecting unknowns for German
-# CONFIG_LOCAL_MAKE=cookbook-repo-addons/config-de-unknowns.mk NEWSPAPER=EXP make all
+# pipenv shell; CONFIG_LOCAL_MAKE=cookbook-repo-addons/config-de-unknowns.mk make all
 
-NEWSPAPER ?= FedGazDe
+NEWSPAPER ?= DTT
 LANGUAGE ?= de
 S3_BUCKET_OCRQA ?= 40-processed-data-sandbox
 PROCESS_LABEL_OCRQA ?= ocrqalexicon
 OCRQA_LANGUAGES_OPTION ?= de
-OCRQA_BLOOMFILTERS_OPTION ?= hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.5-de.bloom 
+OCRQA_BLOOMFILTERS_OPTION ?= hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.6-de.bloom 
 OCRQA_VERBOSE_OUTPUT_OPTION ?= --verbose-output
+OCRQA_MIN_SUBTOKENS_OPTION := --min-subtokens 10
+
+MODEL_ID_OCRQA := wp_v1.0.6

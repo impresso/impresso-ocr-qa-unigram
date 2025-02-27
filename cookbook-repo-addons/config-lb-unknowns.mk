ab5d24d912a55d6ced630b36627748a73f72d5cd
Simon Clematide <simon.clematide@uzh.ch>
2025-02-27 22:52:21 +0100

    fix: Update newspaper name and bloomfilter option for Luxembourgish in config-lb-unknowns.mk

--- cookbook-repo-addons/config-lb-unknowns.mk
+++ cookbook-repo-addons/config-lb-unknowns.mk
@@ -1,10 +1,10 @@
 # configuration file for running the impresso-ocr-qa-unigram pipeline in
 # --verbose-output mode for collecting unknowns for German
 # CONFIG_LOCAL_MAKE=cookbook-repo-addons/config-lb-unknowns.mk  make all
-NEWSPAPER ?= lunion
+NEWSPAPER ?= dunioun
 LANGUAGE ?= lb
 S3_BUCKET_OCRQA ?= 40-processed-data-sandbox
 PROCESS_LABEL_OCRQA ?= ocrqalexicon
 OCRQA_LANGUAGES_OPTION ?= lb
-OCRQA_BLOOMFILTERS_OPTION ?= hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.5-lbr.bloom 
+OCRQA_BLOOMFILTERS_OPTION ?= hf://impresso-project/OCR-quality-assessment-unigram/ocrqa-wp_v1.0.5-lb.bloom 
 OCRQA_VERBOSE_OUTPUT_OPTION ?= --verbose-output
